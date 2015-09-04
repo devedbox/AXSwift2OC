42ca928dd4e248cf16c732a923ae7618417f3dc5
aiXing <862099730@qq.com>
2015-09-04 21:17:46 +0800

    Initial Commit

---
+++ AXSwift2OC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AXSwift2OC
+//
+//  Created by ai on 15/9/4.
+//  Copyright © 2015年 ai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AXSwift2OC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AXSwift2OC
+//
+//  Created by ai on 15/9/4.
+//  Copyright © 2015年 ai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
